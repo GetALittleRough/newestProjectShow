50cbe9c87df3a1af4dcdc1313d408f6a91c61da3
GetALittleRough <260928310@qq.com>
2019-10-29 17:41:20 +0800

    register modal finished

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -37,28 +37,43 @@
                             <!-- <div class="text-center text-muted mb-4">
                                 <small>Or sign up with credentials</small>
                             </div> -->
-                            <form role="form">
+                            <form role="form"
+                                  ref="registerForm"
+                                  :model="registerForm">
                                 <base-input alternative
                                             class="mb-3"
                                             placeholder="昵称（中英文皆可）"
-                                            addon-left-icon="ni ni-circle-08">
+                                            addon-left-icon="ni ni-circle-08"
+                                            v-model="registerForm.username">
                                 </base-input>
                                 <base-input alternative
                                             class="mb-3"
                                             placeholder="邮箱"
-                                            addon-left-icon="ni ni-email-83">
+                                            addon-left-icon="ni ni-email-83"
+                                            v-model="registerForm.mail">
                                 </base-input>
                                 <base-input alternative
                                             type="password"
                                             placeholder="密码"
-                                            addon-left-icon="ni ni-lock-circle-open">
+                                            addon-left-icon="ni ni-lock-circle-open"
+                                            v-model="registerForm.password">
                                 </base-input>
                                 <base-input alternative
-                                            type="repeat-password"
+                                            type="password"
                                             placeholder="重复密码"
-                                            addon-left-icon="ni ni-lock-circle-open">
+                                            addon-left-icon="ni ni-lock-circle-open"
+                                            v-model="registerForm.repeatPassword">
                                 </base-input>
-                                <base-identify></base-identify>
+                                <base-input alternative
+                                            type="text"
+                                            placeholder="验证码"
+                                            addon-left-icon="ni ni-notification-70"
+                                            v-model="registerForm.verification">
+                                
+                                </base-input>
+                                <base-identify 
+                                    @click.native="newCode()"
+                                    :identifyCode="identifyCode"></base-identify>
                                 <!-- <div class="text-muted font-italic">
                                     <small>password strength:
                                         <span class="text-success font-weight-700">strong</span>
@@ -70,7 +85,8 @@
                                     </span>
                                 </base-checkbox>
                                 <div class="text-center">
-                                    <base-button type="primary" class="my-4">创建账户</base-button>
+                                    <base-button type="primary" class="my-4"  @click.native.prevent="createAccount()" data-toggle="modal" data-target="#modal-notification">创建账户</base-button>
+                                    <base-button type="success" class="my-4" @click="goBack()">返回</base-button>
                                 </div>
                             </form>
                         </template>
@@ -78,13 +94,112 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="modal-notification" tabindex="-1" role="dialog" aria-labelledby="modal-notification" aria-hidden="true">
+            <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
+                <div class="modal-content bg-gradient-danger">
+
+                    <div class="modal-header">
+                        <h6 class="modal-title" id="modal-title-notification">Your attention is required</h6>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+
+                        <div class="py-3 text-center">
+                            <i class="ni ni-bell-55 ni-3x"></i>
+                            <h4 class="heading mt-4">You should read this!</h4>
+                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-white">Ok, Got it</button>
+                        <button type="button" class="btn btn-link text-white ml-auto" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+         <modal :show.sync="canRegister"
+                   gradient="danger"
+                modal-classes="modal-danger modal-dialog-centered">
+            <h6 slot="header" class="modal-title" id="modal-title-notification">警告</h6>
+
+            <div class="py-3 text-center">
+                <i class="ni ni-bell-55 ni-3x"></i>
+                <h4 class="heading mt-4">请您注意</h4>
+                <p>表格所填内容不能为空！</p>
+            </div>
+
+            <template slot="footer">
+                <base-button type="white" @click="canRegister=false">好的知道了</base-button>
+                <base-button type="link"
+                                text-color="white"
+                                class="ml-auto"
+                                @click="canRegister=false">
+                    Close
+                </base-button>
+            </template>
+        </modal>
     </section>
 </template>
 <script>
-import { BaseIdentify } from '../components/BaseIdentify'
+import BaseIdentify  from '../components/BaseIdentify.vue'
+import Modal from '../components/Modal'
 export default {
     components: {
-        BaseIdentify
+        BaseIdentify,
+        Modal
+    },
+    computed: {
+        identifyCode: {
+            get: function() {
+                return this.code
+            },
+            set: function() {
+                this.code = this.refreshCode()
+            }
+            
+        }
+    },
+    data() {
+        return {
+            code: this.refreshCode(4),
+            registerForm: {
+                username: '',
+                mail: '',
+                password: '',
+                repeatPassword: '',
+                verification: ''
+            },
+            canRegister: false
+        }
+    },
+    methods: {
+        refreshCode(length) {
+            let randomNum = 0
+            while(randomNum < Math.pow(10, length - 1)) {
+                randomNum = Math.floor(Math.random() * Math.pow(10, length))
+            }
+            return randomNum.toString()
+        },
+        newCode() {
+            this.code = this.refreshCode(4)
+        },
+        goBack() {
+            this.$router.go(-1)
+        },
+        createAccount() {
+            for(let item in this.registerForm) {
+                if(this.registerForm[item] === '') {
+                    this.canRegister = true
+                }
+            }
+            
+        }
     }
 };
 </script>
