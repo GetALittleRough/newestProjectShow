5682580634a5374f7fd3f85c64c0edd16f2939a2
jason <jason@jasons-MacBook-Pro.local>
2019-11-04 09:31:36 +0800

    error in other machine

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,6 @@
 exports.config = {
   serverUrl: "http://localhost:3000",
   bigchaindb: 'http://10.108.84.79:9984',
-  api_path: 'http://localhost:9984/api/v1/'
+  api_path: 'http://10.108.84.79:9984/api/v1/',
+  ipfs_api: '10.108.84.79'
 }
